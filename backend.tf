c7a28eb368f45bb0ab8c69db51c470968b3c8391
Vishwa <Vishwa@g.com>
2026-02-06 17:29:15 +0000

    lab1code

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "tf-state-user4-feb26"
+    key          = "lab1/terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
+    # dynamodb_table = "terraform-locks"
+  }
+}
---
+++ modules/ec2/vars.tf
@@ -0,0 +1,12 @@
+variable "name" { type = string }
+variable "subnet_id" { type = string }
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
